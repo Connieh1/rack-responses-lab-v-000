9381cc2e06fdd44fede482119eea56cc08a9f533
Connie Hyman <constance.hyman@mchsi.com>
2018-11-05 11:33:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,7 @@
 def call(env)
   resp = Rack::Response.new
 
-  num_1 = Kernel.rand(1..20)
-  num_2 = Kernel.rand(1..20)
-  num_3 = Kernel.rand(1..20)
-
-  resp.write "#{num_1}\n"
-  resp.write "#{num_2}\n"
-  resp.write "#{num_3}\n"
+  time = Time.now
 
   if time.between? 24:00 & 12:00
     resp.write "Good Morning!"
