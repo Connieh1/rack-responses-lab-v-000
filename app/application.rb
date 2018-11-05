8dca6b56978fb16e97645a748c3dcfc99e14eaf1
Connie Hyman <constance.hyman@mchsi.com>
2018-11-05 11:31:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,9 @@ def call(env)
   resp.write "#{num_3}\n"
 
   if time.between? 24:00 & 12:00
-    resp.write "Good Morning"
+    resp.write "Good Morning!"
   else
-    resp.write "Good Afternoon"
+    resp.write "Good Afternoon!"
   end
 
   resp.finish
